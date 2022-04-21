8419ff33b63b40a36d6b2b8695fd152e0cb81fcd
vittorioaquilino <vittorioaquilino17@gmail.com>
2022-04-21 09:52:31 +0200

    add do while

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,16 +1,20 @@
 // jsnack3. Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente.
 
 // chiedo un numero da inserire all'utene
-const userNumber = parseInt(prompt("inserisci un numero"));
-console.log("numero inserito", userNumber);
+let userNumber;
+do {
+    userNumber = parseInt(prompt("inserisci un numero"));
+    console.log("numero inserito", userNumber);
+} while (userNumber < 1 || isNaN(userNumber))
 
 // creo un array vuoto
 const numberList = [];
 
 // inizio il ciclo
-for (let i = 0; i <= userNumber; i++) {
+for (let i = 1; i <= userNumber; i++) {
     // faccio il cubo del numero
-    cube = i ** 3;
+    // const cube = i ** 3; > prima soluzione 
+    const cube = Math.pow(i, 3);
     console.log("cubo", cube);
     // inserisco i cubi all'interno dell'array
     numberList.push(cube);
